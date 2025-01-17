f25b7499aad9dd5a309bc132906c3b7f62b0e6c4
Kai <kai.zahn@yahoo.de>
2025-01-17 18:19:48 +0100

    Fix crash and test timer staying after sculptor dies - dec52a7c

--- src/main/java/com/bobmowzie/mowziesmobs/server/bossinfo/BossInfoSculptor.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/bossinfo/BossInfoSculptor.java
@@ -19,12 +19,17 @@ public class BossInfoSculptor extends MMBossInfoServer {
 
     public void update() {
         if (sculptor.isTesting() && !sculptor.isTestPassed() && !sculptor.isTestFailed()) {
-            this.setProgress(1.0f - this.sculptor.getTestTimePassed() / (float) this.sculptor.getMaxTestTime());
-            if (this.sculptor.getTestingPlayer() instanceof ServerPlayer) {
-                this.addPlayer((ServerPlayer) this.sculptor.getTestingPlayer());
+            if (sculptor.isRemoved() || sculptor.isDeadOrDying()) {
+                setVisible(false);
             }
-            setDisplayName(TESTING_NAME);
-            setVisible(true);
+            else {
+                this.setProgress(1.0f - this.sculptor.getTestTimePassed() / (float) this.sculptor.getMaxTestTime());
+                if (this.sculptor.getTestingPlayer() instanceof ServerPlayer) {
+                    this.addPlayer((ServerPlayer) this.sculptor.getTestingPlayer());
+                }
+                setDisplayName(TESTING_NAME);
+                setVisible(true);
+            }
         }
         else {
             setVisible(sculptor.hasBossBar() && sculptor.isFighting());
