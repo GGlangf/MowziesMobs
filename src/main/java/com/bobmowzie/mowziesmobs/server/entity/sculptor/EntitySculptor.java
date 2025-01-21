ed769a9bc81c2280f706dbbdf880e28525a7dda8
Kai <kai.zahn@yahoo.de>
2025-01-21 08:39:31 +0100

    Bluff death animation tweaks - a2a926fd

--- src/main/java/com/bobmowzie/mowziesmobs/server/entity/sculptor/EntitySculptor.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/entity/sculptor/EntitySculptor.java
@@ -417,6 +417,8 @@ public class EntitySculptor extends MowzieGeckoEntity {
     public void tick() {
         setDeltaMovement(0, getDeltaMovement().y, 0);
         super.tick();
+        setDeltaMovement(0, getDeltaMovement().y, 0);
+
         if (testingPlayer == null && getTestingPlayerID().isPresent()) {
             testingPlayer = level().getPlayerByUUID(getTestingPlayerID().get());
             if (testingPlayer != null) testing = true;
@@ -822,6 +824,11 @@ public class EntitySculptor extends MowzieGeckoEntity {
         return player != null
                 && (canAttack(player) || isTesting())
                 && distanceTo(player) < 2500;
+    }
+
+    @Override
+    public boolean canBeLeashed() {
+        return false;
     }
 
     public static class StartTestAbility extends Ability<EntitySculptor> {
